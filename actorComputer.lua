e7563d74178b9034081a83e367f80478ddf4d103
SenorWerner <130681536+SenorWerner@users.noreply.github.com>
2026-08-08 13:37:54 +0200

    Update actorComputer.lua
    aktuekke Verison aus Minecraft. Vorbereitet für Status "moving".

--- actorComputer.lua
+++ actorComputer.lua
@@ -2,61 +2,73 @@
 local modem = peripheral.find("modem")
 if not modem then error("Kein Modem gefunden") end
 rednet.open(peripheral.getName(modem))
-
-rednet.host("computer", "gate_001")
-
+ 
+local HOST_ID = "gate_002"
+ 
+rednet.host("computer", HOST_ID)
+ 
 print("Gate-System gestartet...")
-
+ 
+local currentAction = "deactivate"
+ 
+ 
 -- === CONFIG ===
 local PULSE_TIME = 0.5
-
+ 
 -- === STATUS FUNCTION ===
-local function getStatus()
-    if redstone.getInput("right") then
+local function getStatus(action)
+    if redstone.getInput("right") and action == "activate" then
         return "active"
-    else
+    elseif redstone.getInput("right") == false and action == "activate" then
+        return "moving"
+    elseif redstone.getInput("right") and action == "deactivate" then
+       return "moving"
+    else 
         return "inactive"
     end
 end
-
+ 
 -- === ACTIONS ===
 local function activate()
     redstone.setOutput("back", true)
 end
-
+ 
 local function deactivate()
-    redstone.setOutput("front", true)
+    redstone.setOutput("back", false)
 end
-
+ 
 local function pulse()
     redstone.setOutput("left", true)
     sleep(PULSE_TIME)
     redstone.setOutput("left", false)
 end
-
+ 
 -- === MAIN LOOP ===
 while true do
     local sender, msg = rednet.receive()
-
-    if type(msg) == "table" and msg.id == "gate_001" and msg.action then
+ 
+    if type(msg) == "table" and msg.id == HOST_ID and msg.action then
         
         if msg.action == "activate" then
+            currentAction = "activate"
             activate()
-
+ 
         elseif msg.action == "deactivate" then
+            currentAction = "deactivate"
             deactivate()
-
+ 
         elseif msg.action == "pulse" then
             pulse()
-
+ 
         elseif msg.action == "status" then
             -- nichts ausführen, nur Status zurückgeben
         end
-
+ 
         -- === STATUS ANTWORT ===
         rednet.send(sender, {
-            id = "gate_001",
-            status = getStatus()
+            id = HOST_ID,
+            status = getStatus(currentAction)
         })
+        print(getStatus(currentAction), currentAction)
     end
 end
