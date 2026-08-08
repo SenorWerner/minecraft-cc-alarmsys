e1b6ed729e1a6298dbc3c5a5b580fb7bf95de3f6
SenorWerner <130681536+SenorWerner@users.noreply.github.com>
2026-08-08 20:47:43 +0200

    Update communcationComputer.lua
    import aus minecraft

--- communcationComputer.lua
+++ communcationComputer.lua
@@ -2,34 +2,41 @@
 local modem = peripheral.find("modem")
 if not modem then error("Kein Modem gefunden") end
 rednet.open(peripheral.getName(modem))
-
+ 
+rednet.host("computer", "comm_001")
+ 
 print("CommunicationComputer gestartet...")
-
+print("Unter ID:", rednet.lookup("computer", "comm_001"))
+ 
 -- === CACHE ===
 -- Speichert bekannte Systeme (optional für Performance)
 local systemCache = {}
-
+ 
 local function getSystemID(label)
     if systemCache[label] then
         return systemCache[label]
     end
-
+ 
     local id = rednet.lookup("computer", label)
     if id then
+        print("Computer found")
+        systemCache[label] = id
+    else
+        id = rednet.lookup("turtle" , label)
+        print("turtle foudnd")
         systemCache[label] = id
     end
-
     return id
 end
-
+ 
 -- === MAIN LOOP ===
 while true do
     local sender, msg = rednet.receive()
-
+ 
     if type(msg) == "table" and msg.id and msg.action then
         local targetLabel = msg.id
         local targetID = getSystemID(targetLabel)
-
+ 
         if not targetID then
             print("System nicht gefunden: " .. targetLabel)
             -- optional: Fehler zurücksenden
@@ -40,12 +47,12 @@ while true do
         else
             -- Weiterleiten an Zielsystem
             rednet.send(targetID, msg)
-
-            -- Wenn Status angefragt → Antwort zurückleiten
+            print(targetID, msg.id, msg.action)
+            -- Wenn Status angefragt ? Antwort zurückleiten
             if msg.action == "status" then
                 -- Warte auf Antwort vom System
                 local replySender, reply = rednet.receive(2)
-
+                
                 if reply and type(reply) == "table" and reply.status then
                     -- zurück an ursprünglichen Sender
                     rednet.send(sender, reply)
