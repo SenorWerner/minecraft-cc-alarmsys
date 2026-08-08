8a3dc38d748b4800678cb1ed695c68b4d2846195
SenorWerner <130681536+SenorWerner@users.noreply.github.com>
2026-08-08 20:53:14 +0200

    Create config.lua
    Beispiel config

---
+++ config.lua
@@ -0,0 +1,7 @@
+return {
+    HOST_ID = "button_interlock_crisisroom_001", -- ID von diesem Computer
+    HOST_NAME = "Lift&Tür blockieren", -- sprechender Name von diesem Computer (nur zur Anzeige))
+    COMM_SERVER_ID = "comm_001", -- ID vom Communication Server
+    TARGET_ID = "interlock_crisisroom_001", -- ID vom Computer, welcher ferngesteuert werden soll
+    SENSOR_ID = "interlock_crisisroom_001" -- ID von Sensor Computer oder Turtle zur Status abfrage. Wenn der Status über die das das Target-System abgefargt werden soll, dann hier TARGET_ID eintragen
+}
