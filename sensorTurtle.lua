5163d03ada223f0f028b86e2e253a3f753853de0
SenorWerner <130681536+SenorWerner@users.noreply.github.com>
2026-08-08 20:49:06 +0200

    Update sensorTurtle.lua
    import aus minecraft

--- sensorTurtle.lua
+++ sensorTurtle.lua
@@ -2,42 +2,45 @@
 local modem = peripheral.find("modem")
 if not modem then error("Kein Modem gefunden") end
 rednet.open(peripheral.getName(modem))
-
+ 
 local config = require("config")
-
+ 
 local HOST_ID = config.HOST_ID
-
+ 
 rednet.host("turtle", HOST_ID)
-
+ 
 print("Sensor-Turtle gestartet...")
-
+ 
 -- === STATUS FUNCTION ===
 local function getStatus()
-    local front = turtle.detect()
-    local up = turtle.detectUp()
-    local down = turtle.detectDown()
-
-    return {
-        front = front,
-        up = up,
-        down = down
-    }
-end
-
+    if turtle.detect() then
+        return "active"
+    else
+        return "inactive"
+    end
+  end
+ 
 -- === MAIN LOOP ===
 while true do
     local sender, msg = rednet.receive()
-
-    if type(msg) == "table" and msg.id == HOST_ID and msg.action == "status" then
+    print(sender, type(msg), msg.id, msg.action)
+        if type(msg) == "table" and msg.id == HOST_ID and msg.action == "status" then
         
         local status = getStatus()
-
-        rednet.send(sender, {
-            id = HOST_ID,
-            status = status
-        })
-
+        
+        replyMsg={
+            id =HOST_ID, 
+            action = "status",
+            status = getStatus()
+        }
+        
+        rednet.send(sender, replyMsg)
+ 
         print("Status gesendet:")
-        print("Front:", status.front, "Up:", status.up, "Down:", status.down)
+ 
+        print("An:", sender)
+        print("Host_ID:", replyMsg.id)
+        print("status:", replyMsg.status)
+        print("action:" , replyMsg.action)
     end
 end
