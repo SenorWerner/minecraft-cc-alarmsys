27137f6be2a724a266243602bd8c111114a7edc9
SenorWerner <130681536+SenorWerner@users.noreply.github.com>
2026-08-10 13:15:44 +0200

    Create run.lua

---
+++ os/os/run.lua
@@ -0,0 +1,29 @@
+local network = require("os.network")
+local sm = require("os.service_manager")
+
+local modem = peripheral.find("modem")
+rednet.open(peripheral.getName(modem))
+
+local config = require("config.config")
+rednet.host("computer", config.HOSTNAME)
+
+local status = require("config.service_status")
+
+for _, file in ipairs(fs.list("services")) do
+    local name = file:gsub(".lua", "")
+    local service = require("services." .. name)
+
+    if status[name] then
+        if service.protocol then
+            network.register(service.protocol, service.handle)
+        end
+        if service.run then
+            sm.add(service.run)
+        end
+    end
+end
+
+parallel.waitForAll(
+    network.run,
+    sm.run
+)
