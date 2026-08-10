7148a17c69bd12fc846db2949f69578e67cbdb9f
SenorWerner <130681536+SenorWerner@users.noreply.github.com>
2026-08-10 13:19:20 +0200

    Create heartbeat.lua

---
+++ os/services/heartbeat.lua
@@ -0,0 +1,20 @@
+local config = require("config.config")
+
+local function run()
+    while true do
+        local id = rednet.lookup("dns", config.DNS_SERVER)
+
+        if id then
+            rednet.send(id, {
+                action = "heartbeat",
+                name = config.HOSTNAME
+            }, "ns")
+        end
+
+        sleep(5)
+    end
+end
+
+return {
+    run = run
+}
