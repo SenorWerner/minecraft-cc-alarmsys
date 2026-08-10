e7914eb6e6b130382cfbeccd3e41b9f3c926ac3c
SenorWerner <130681536+SenorWerner@users.noreply.github.com>
2026-08-10 13:20:05 +0200

    Create update_listener.lua

---
+++ os/services/update_listener.lua
@@ -0,0 +1,20 @@
+local updater = require("updater")
+
+local function handle(sender, msg)
+    if msg.action ~= "update" then return end
+
+    local ok, err = updater.run(msg)
+
+    rednet.send(sender, {
+        action = "update_result",
+        ok = ok,
+        error = err
+    }, "update")
+
+    if ok then os.reboot() end
+end
+
+return {
+    protocol = "update",
+    handle = handle
+}
