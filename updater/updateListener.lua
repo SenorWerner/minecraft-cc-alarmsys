1ed596d2992455d7f63728305e668ee87d69d99b
SenorWerner <130681536+SenorWerner@users.noreply.github.com>
2026-08-10 11:33:23 +0200

    Update updateListener.lua

--- updater/updateListener.lua
+++ updater/updateListener.lua
@@ -1,27 +1,27 @@
 local updater = require("updater")
 
-local function updateListener()
-    while true do
-        local sender, msg, protocol = rednet.receive("update")
+local function handle(sender, msg, protocol)
+    if type(msg) ~= "table" then return end
+    if msg.action ~= "update" then return end
 
-        if type(msg) == "table" and msg.action == "update" then
-            print("Update empfangen")
+    print("Update empfangen")
 
-            local ok, err = updater.run(msg)
+    local ok, err = updater.run(msg)
 
-            rednet.send(sender, {
-                action = "update_result",
-                ok = ok,
-                error = err
-            }, "update")
+    rednet.send(sender, {
+        action = "update_result",
+        ok = ok,
+        error = err
+    }, "update")
 
-            if ok then
-                print("Neustart...")
-                sleep(1)
-                os.reboot()
-            end
-        end
+    if ok then
+        print("Neustart...")
+        sleep(1)
+        os.reboot()
     end
 end
 
-parallel.run(updateListener, main)
+return {
+    protocol = "update",
+    handle = handle
+}
