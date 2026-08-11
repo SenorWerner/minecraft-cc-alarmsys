a1b2c800ee26c7d8ce0a12215f391e4d1c6911db
SenorWerner <130681536+SenorWerner@users.noreply.github.com>
2026-08-11 09:19:38 +0200

    Update config_gateDriver.lua

--- driver/gateDriver/config_gateDriver.lua
+++ driver/gateDriver/config_gateDriver.lua
@@ -1,3 +1,24 @@
 return{
+  CORE_CONFIG_PATH = "/config/config", -- standard PATH "/config/config", only alter when not running on alarmSysOS
+  PROTOCOL = "control", -- standard for actors and sensor is "control"
+  
+--Feedback config
+  
+  FEEDBACK_ACTIVE_ON = true, 
+  FEEDBACK_INACTIVE_ON = true,
+  FEEDBACK_SIDE_ACTIVE = "right", 
+  FEEDBACK_SIDE_INACTIVE = "left",
+
+--Status Strings
+  
+  FEEDBACK_RETURN_STRING_ACTIVE = "active",
+  FEEDBACK_RETURN_STRING_INACTIVE = "inactive",
+  FEEDBACK_RETURN_STRING_NEITHER = "moving",
+  FEEDBACK_RETURN_STRING_ELSE = "error",
+
+  --Action config
+
+  ACTIVATE_REDSTONE_SIDE = "", -- if empty -> no side used
+  PULSE_REDSTONE_SIDE = "back", 
   PULSE_TIME = 1
 }
